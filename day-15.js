b59e65d148ed73a05cf2064f8ef69a76420b6889
samiksha-3010 <samiksha30102@gmail.com>
2023-05-28 16:17:06 +0530

    updates

---
+++ day-15.js
@@ -0,0 +1,65 @@
+// datatypes string number Boolean
+// console.log()
+// typesof()
+// varibules
+// conditinoal statement
+
+
+
+
+
+// var number1 =10;
+// var number2=20;
+
+// var addition = number1+  number2
+// console.log(addition)
+
+
+// var sub = number1 -number2;
+// console.log (sub) 
+
+// var mul = number1*number2;
+// console.log (mul)
+
+// var divd = number1 /number2;
+// console.log(divd)
+
+// var i = 1;
+// console.log(1)
+// i = i +1
+// i++;
+// i++;
+// i++;
+// console.log(i)
+
+
+// var number= 100;
+// console.log ( number > 150)
+
+
+ conditional statement
+1. if else
+ 2. switch
+
+// 1.if else
+
+// if (condition) {
+//     code  to be excute
+//     console.log("condition true") 
+
+// } if else (condition second)
+// code to be excute
+
+// console.log ("condition true") 
+
+// if else (condition second)
+// code to be excute
+// console .log ("condition true")
+
+
+
+// question.  check given number is lesser than 100 or 150 or 150
+
+// ver number =98763;
+// console.log (number >5)
+// if (number> 100)
---
+++ day-16.js
@@ -0,0 +1,36 @@
+// var myName  ; //Decleration
+// console.log(myName) ;
+// console.log(typeof(myName))
+
+// myName = "samiksha"; assign
+// console.log(myName,//"assgin")
+
+
+// //Array
+// var hii ="hii"
+// var kuchbhi=["samiksha","xyz",8976,true,"false",hii];
+// console.log(kuchbhi[0]);
+// console.log(kuchbhi.length)
+
+// Loops-for
+
+//print number for 1-10
+
+var i  = 1;
+i++;
+console.log(i)
+
+console.log(1)
+console.log(2)
+console.log(3)
+console.log(4)
+console.log(5)
+
+// for
+// if () {}
+
+// for (initial condition; ending condition; steps ) { code to be execute }
+// for (var i = 1001; 1001 <= 1000; i++ ) { print("execute") }
+for(var i = 1<= 100; i++){
+
+}
