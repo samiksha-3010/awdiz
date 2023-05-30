ae976f97f20b88c0cb507372f14dc7d237327545
samiksha-3010 <samiksha30102@gmail.com>
2023-05-30 11:32:24 +0530

    updates

--- day-15.js
+++ day-15.js
@@ -1,65 +1,100 @@
-// datatypes string number Boolean
-// console.log()
-// typesof()
-// varibules
-// conditinoal statement
+// var number = 10;
+// var number = 20;
 
-
-
-
-
-// var number1 =10;
-// var number2=20;
-
-// var addition = number1+  number2
+// var addition = number1 + number2;
 // console.log(addition)
 
+// var sub = number1 - number2;
+// console.log(sub)
 
-// var sub = number1 -number2;
-// console.log (sub) 
+// var mul = number1 * number2;
+// console.log(mul)
 
-// var mul = number1*number2;
-// console.log (mul)
+// var divid = number1 / number2;
+// console.log(divid)
 
-// var divd = number1 /number2;
-// console.log(divd)
-
-// var i = 1;
+// var i =1;
 // console.log(1)
-// i = i +1
+// i = i +1;
 // i++;
 // i++;
 // i++;
-// console.log(i)
+// i++;
+// console.log(1)
+
+var number = 100;
+console.log(number > 150)
+
+// Conditional Statement
+
+// 1. if else 
+// 2. switch
+
+// 1. if else
+//     if (conditions) {
+//         code to be execute
+//         console.log("condition true")
+//     } else if (condtions second) {
+//         code to be execute
+//     console.log("condition true")
+//     }  else if (condtions second) {
+//             code to be execute
+//         console.log("condition true")
+//     }else {
+//         code to be execute
+//     console.log("condition false")
+//     }
+
+// Q. Check given number is lesser than 100 or 150 or 200;
+
+// var number = 98763;
+// console.log(number < 5)
+// if (number < 100) {
 
 
-// var number= 100;
-// console.log ( number > 150)
+// console.log("number is leser than 100.")
+// }else if ( number < 150) {
+//     console.log("number is leser than 150 and grater than 150....")
+// } else if (number < 98765){
+//     console.log("number is grater than 98765")
+// }
+// Q. Check user age and provied licennnn... 
+
+// if user age is 18 then he is applicbke for Learnign licc... 
+//  user age is greater than 18 and lesser than 80 then dl 
+//   user age is lesser 18 or greter 80 not allowed dl  
+
+// var userAge = 25;
+
+// if (userAge == 18) {
+//     console.log("You are applicable for Learning Licence...")
+// } else if( userAge < 80 && userAge > 18 ){
+//     console.log("You are allow for Driving Licence..")
+// } else {
+//     console.log("You are NOT allow for driving licenceee...")
+// }
 
 
- conditional statement
-1. if else
- 2. switch
+ var number1 = 123;
+ var number2 = "123";
+ console.log(num1 == num2)
+ console.log(num1 === num2)
 
-// 1.if else
-
-// if (condition) {
-//     code  to be excute
-//     console.log("condition true") 
-
-// } if else (condition second)
-// code to be excute
-
-// console.log ("condition true") 
-
-// if else (condition second)
-// code to be excute
-// console .log ("condition true")
+ var num1 = 98;
+ console.log(num1 > 50 && num1 < 99 && num1 < 10)
+console.log(num1 > 50 || num1 < 90 || num1 < 20)
 
 
+//  datatypes
+//  string, number, boolens
+//  varibles
+//  console.log();
+//  typeof();
+//  condition statement if else
+//  && ||
+//  + ,-,*,/
+//  =, == , ===
+//  ++
+//  <> < = > =
 
-// question.  check given number is lesser than 100 or 150 or 150
 
-// ver number =98763;
-// console.log (number >5)
-// if (number> 100)
