d4e8c7509236c37f625415eee6713982f45af5c9
samiksha-3010 <samiksha30102@gmail.com>
2023-05-31 12:41:00 +0530

    Update day-16.js

--- day-16.js
+++ day-16.js
@@ -16,21 +16,75 @@
 
 //print number for 1-10
 
-var i  = 1;
-i++;
-console.log(i)
+// var i  = 1;
+// i++;
+// console.log(i)
 
-console.log(1)
-console.log(2)
-console.log(3)
-console.log(4)
-console.log(5)
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// console.log(4)
+// console.log(5)
 
 // for
-// if () {}
+// // if () {}
 
-// for (initial condition; ending condition; steps ) { code to be execute }
-// for (var i = 1001; 1001 <= 1000; i++ ) { print("execute") }
-for(var i = 1<= 100; i++){
+// for (initial condition; ending condition; steps) { code to be execute }
 
+//  for (var i = 1001; 1001 <= 1000; i++ ) { print("execute") }
+
+//  for (var i = 1; i <= 100; i++) {
+//     console.log("Hello", i);
+// }
+
+//     for(var i = 1; i<= 100; i+i+2) {
+//         console.log("Hello" , i);
+//     }
+//     for (var i = 234; i <= 765; i++) {
+//         console.log("Hello", i);
+//     }
+    
+//     for (var i = 50 ; i<= 230; i=i *2){
+//         console.log("Hello",i);
+//     }
+//     for (var i =1 ;i <=1000; i=i +6 ){
+//         console.log("Hello",i);
+
+//     }
+//      for(var i= 8976; i <=87568; i++){
+
+//     console.log("Hello" ,i);
+
+//     }
+//     for(var i = 5;i <= 1000; i=i ** 2 ){
+//         console.log("Hello" ,i);
+//     }
+     
+// Q. Print number from 10 -1; 
+for(var i= 10; i >= 1;i --){
+    console.log(i)
+}
+
+for(var i= 100;i >=54; i -- ){
+    console.log(i)
+}
+for(var i= 100;i >= 1; i=i -2){
+    console.log(i)
+}
+for(var i=1000;i >=1; i=i -10 ){  
+console.log(i)
+}
+for(var i= 1000/2;i >=1; i=i -10){
+    console.log(i)
+}
+var remote =[ "s" , 'b', "p", "c", "y", 87, 987,true ];
+
+console.log (remote[0])
+console.log (remote[1])
+console.log (remote[2])
+console.log (remote[3])
+console.log (remote[4])
+
+for(var i = 0; i < remote.length; i++){
+    console.log(remote[i])
 }
