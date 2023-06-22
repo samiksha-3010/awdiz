a5df7a7b3fa8b7bd76d22f2e5c59624f54905676
samiksha-3010 <samiksha30102@gmail.com>
2023-06-22 13:15:33 +0530

    update

--- day-30.js
+++ day-30.js
@@ -11,26 +11,26 @@ var array = [ {  id: 1, name: "samiksha", age: 25, surname: "yadav" },
 
   })
 
-var username = ' samikshayadav'
-var index = array.findIndex((object) => object.name == username);
-console.log(index);
+// var username = ' samikshayadav'
+// var index = array.findIndex((object) => object.name == username);
+// console.log(index);
 
 
-var UserName = 'samiksha';
-var selectedUser = array.find((kuchBhi) => kuchBhi. name == UserName);
-console.log(selectedUser)
+// var UserName = 'samiksha';
+// var selectedUser = array.find((kuchBhi) => kuchBhi. name == UserName);
+// console.log(selectedUser)
 
-var userId = 3;
-var selectedUser = array .find ((samiksha => samiksha.id == userId))
-console.log(selectedUser);
+// var userId = 3;
+// var selectedUser = array .find ((samiksha => samiksha.id == userId))
+// console.log(selectedUser);
 
 
-var  userAge = 22;
-var  selectedUser = array.find ((object => object.age == userAge));
-console.log( selectedUser)
+// var  userAge = 22;
+// var  selectedUser = array.find ((object => object.age == userAge));
+// console.log( selectedUser)
 
-var numbers = [1,2,3,5,7,6,4,9,67,23];
-console.log(numbers.sort())
+// var numbers = [1,2,3,5,7,6,4,9,67,23];
+// console.log(numbers.sort())
 
 
 var numbers = [1, 8, 9, 15, 3, 7, 37, 2, 6, 1, 6, 7, 9];
@@ -43,8 +43,8 @@ const words = [ 'anika', 'disha', 'vibha', 'nidhiyada','destruction', 'present '
 const result = words.filter(words => words.length <6);
 console.log(result)
 
-const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present', 'samikshayadav'];
+// const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present', 'samikshayadav'];
 
-const result = words.filter(word => word.length < 8);
+// const result = words.filter(word => word.length < 8);
 
-console.log(result);
+// console.log(result);
