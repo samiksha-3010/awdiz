07e367b07f33a46e484ad3860a71e3d716d6e1c3
samiksha-3010 <samiksha30102@gmail.com>
2023-11-01 10:47:21 +0530

    done

--- day-16.js
+++ day-16.js
@@ -77,14 +77,40 @@
 // for(var i= 1000/2;i >=1; i=i -10){
 //     console.log(i)
 // }
-var remote =[ "s" , 'b', "p", "c", "y", 87, 987,true ];
+// var remote =[ "s" , 'b', "p", "c", "y", 87, 987,true ];
 
-console.log (remote[0])
-console.log (remote[1])
-console.log (remote[2])
-console.log (remote[3])
-console.log (remote[4])
+// console.log (remote[0])
+// console.log (remote[1])
+// console.log (remote[2])
+// console.log (remote[3])
+// console.log (remote[4])
 
-for(var i = 0; i < remote.length; i++){
-    console.log(remote[i])
- }
+// for(var i = 0; i < remote.length; i++){
+//     console.log(remote[i])
+//  }
+
+
+var fruits = ["orange", "banana", 'carrot', "papaya", "grapes"]
+
+fruits.shift(); // it removes first element of array
+fruits.shift();
+fruits.shift();
+console.log(fruits)
+
+
+var numbers = [10, 22, 42, 51, 63, 73, 84];
+numbers.unshift(87,76)
+numbers.unshift(10) // it adds element to start of an array
+console.log(numbers)
+
+for(var i = 10 ; i <= 25; i++){
+    console.log(i)
+}
+
+for (var i = 10; i < 100; i += 20) {
+    console.log(i)
+}
+
+for (var i = 1; i <= 10; i++) { 
+    console.log(i) 
+}
