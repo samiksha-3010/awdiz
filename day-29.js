ac28b1ff969a8b1a067330d74dba222e48300a50
samiksha-3010 <samiksha30102@gmail.com>
2023-11-15 02:16:43 +0530

    done

--- day-29.js
+++ day-29.js
@@ -1,110 +1,110 @@
-// async  await
+async  await
 
 
-// async function myFuncation(){
-//     console.log("Before going for function");
-//     const res = await whoTakesTime();
-//     console.log(res,"-res");
-//     console.log("after funcation");
-// }
-// myFuncation();
+async function myFuncation(){
+    console.log("Before going for function");
+    const res = await whoTakesTime();
+    console.log(res,"-res");
+    console.log("after funcation");
+}
+myFuncation();
 
 
-// const seconds = new Date().getTime() / 1000;
-// console.log(seconds)
+const seconds = new Date().getTime() / 1000;
+console.log(seconds)
 
 
-// var twoSum = function(nums, target){
-//     console.log("hello")
-// };
-// function  hello(){
-//     const hi = () =>{}
-// }
-// o(n)
-// for(){}
-// for(){}
+var twoSum = function(nums, target){
+    console.log("hello")
+};
+function  hello(){
+    const hi = () =>{}
+}
+o(n)
+for(){}
+for(){}
 
-// var totalNumber = 0;
-// for (var i = 0; i < 10; i++){
-//     for (var j = 0; j <10; j++){
-//         totalNumber += 1
-//     }
-// }
-// console.log (totalNumber);
+var totalNumber = 0;
+for (var i = 0; i < 10; i++){
+    for (var j = 0; j <10; j++){
+        totalNumber += 1
+    }
+}
+console.log (totalNumber);
 
-// var totalNumber = 0;
-// for( var i = 0; i  <20; i++ ){
-//     for(var j = 0; j <20; j++){
-//         totalNumber +=1
-//     }
-// }
-// console.log(totalNumber);
+var totalNumber = 0;
+for( var i = 0; i  <20; i++ ){
+    for(var j = 0; j <20; j++){
+        totalNumber +=1
+    }
+}
+console.log(totalNumber);
 
-// var totalName = "samiksha";
-// for(var i="samiksha"; i<10;i++){
-//     for (var j="samiksha"; j<10;j++){
-//        totalName +=10 
-//     }
-// 
-// console.log("totalName");
+var totalName = "samiksha";
+for(var i="samiksha"; i<10;i++){
+    for (var j="samiksha"; j<10;j++){
+       totalName +=10 
+    }
 
-
-// var mycity = "mumbai"
-
-// function Awdiz(name , number, age , ){
-// this.name = name;
-// this.number = number;
-// this.age = age;
-// this.adress = mycity
-
-// }
-// const student1 = new Awdiz('samiksha','34892992', '98766', );
-// console.log(student1);
-// console.log();
-
-// function shopping( cloth, fruits,furniture){
-//     this.cloth = cloth;
-//     this.fruits = fruits;
-//     this.furniture = furniture;
-// }
-// const person1 = new shopping('frocksuits', 'mango','table');
-// console.log(person1);
-// console.log();
-
-// var res = []; // 1, 2,3,4
-// var nums = [2,2,2,3,3,4,4,6,6,9];
-// for (let i=0; i< nums.length; i++){
-//     if (nums[i]   != nums[i+1] )
-//     res.push(nums[i])
-
-//  }
-
-// console.log(res);
+console.log("totalName");
 
 
-// var res =[]; 
-// var nums = [1,1,1,2,2,3,3,4,4,5,5,6];
-// for(let i=0;i<nums.length; i++){
-//     if(nums[i] != nums[i+1]){
-//         res.push(nums[i]);
-//     }
+var mycity = "mumbai"
 
-// }
-// console.log(res);
+function Awdiz(name , number, age , ){
+this.name = name;
+this.number = number;
+this.age = age;
+this.adress = mycity
+
+}
+const student1 = new Awdiz('samiksha','34892992', '98766', );
+console.log(student1);
+console.log();
+
+function shopping( cloth, fruits,furniture){
+    this.cloth = cloth;
+    this.fruits = fruits;
+    this.furniture = furniture;
+}
+const person1 = new shopping('frocksuits', 'mango','table');
+console.log(person1);
+console.log();
+
+var res = []; // 1, 2,3,4
+var nums = [2,2,2,3,3,4,4,6,6,9];
+for (let i=0; i< nums.length; i++){
+    if (nums[i]   != nums[i+1] )
+    res.push(nums[i])
+
+ }
+
+console.log(res);
 
 
+var res =[]; 
+var nums = [1,1,1,2,2,3,3,4,4,5,5,6];
+for(let i=0;i<nums.length; i++){
+    if(nums[i] != nums[i+1]){
+        res.push(nums[i]);
+    }
 
+}
+console.log(res);
 
 
 
 
 
-// var kuchbhi = '12345';
-// console.log("Data here-", kuchbhi)
-// console.log("Datatype -", typeof(kuchbhi));
-// var kuchbhi = 12345;
-// console.log("Data here-", kuchbhi)
-// console.log("Datatype -", typeof(kuchbhi));
+
+
+
+var kuchbhi = '12345';
+console.log("Data here-", kuchbhi)
+console.log("Datatype -", typeof(kuchbhi));
+var kuchbhi = 12345;
+console.log("Data here-", kuchbhi)
+console.log("Datatype -", typeof(kuchbhi));
 
 
 // var booleanValue = 'false';
